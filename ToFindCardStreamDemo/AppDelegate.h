13cd84db4ccd1b0bbf573baa718ac09923896da5
e29hz <ehongzhen@gmail.com>
2016-09-01 10:41:29 +0800

    Initial Commit

---
+++ ToFindCardStreamDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToFindCardStreamDemo
+//
+//  Created by 鄂鸿桢 on 16/9/1.
+//  Copyright © 2016年 e29hz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToFindCardStreamDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToFindCardStreamDemo
+//
+//  Created by 鄂鸿桢 on 16/9/1.
+//  Copyright © 2016年 e29hz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
